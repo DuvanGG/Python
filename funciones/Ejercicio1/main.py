8d52291c332c6e429febd722a9c2301b1c0de0ac
DuvanGG <duvanga@hotmail.es>
2022-05-01 09:25:41 -0500

    Ejercicio area circulo

--- funciones/Ejercicio1/main.py
+++ funciones/Ejercicio1/main.py
@@ -1,5 +1,15 @@
-def areaTriangulo(altura, base):
-    return (base * altura) / 2
+import math
 
-result = areaTriangulo(10, 18)
-print(result)
+def areaTriangulo(altura, base):
+    return (float(base) * float(altura)) / 2
+
+resultadoTriangulo = areaTriangulo(10.5, 18)
+
+
+def areaCirculo(radio):
+    return (math.pi * (radio**2))
+
+resultadoCirculo = areaCirculo(5)
+
+print("El area del triangulo es: ", resultadoTriangulo)
+print("El area del circulo es: ", resultadoCirculo)
