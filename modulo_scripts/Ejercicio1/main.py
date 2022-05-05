7e7bb27253adb17f0aa8354debd74f7ddec7c103
DuvanGG <duvanga@hotmail.es>
2022-05-05 08:55:46 -0500

    Ejercicio de modulos

---
+++ modulo_scripts/Ejercicio1/main.py
@@ -0,0 +1,6 @@
+import operaciones as o
+
+print(f'la suma es: {o.sumar(3,6)}')
+print(f'la resta es: {o.restar(6,3)}')
+print(f'la multiplicación es: {o.multiplicar(3,6)}')
+print(f'la divición es: {o.dividir(6,3)}')
