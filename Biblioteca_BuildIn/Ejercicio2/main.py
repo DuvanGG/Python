decb853bf7415baa3e45283aae3f260faea1397c
DuvanGG <duvanga@hotmail.es>
2022-05-20 13:59:47 -0500

    Ejercicio 2 numeros impares y suma con filter y reduce

---
+++ Biblioteca_BuildIn/Ejercicio2/main.py
@@ -0,0 +1,13 @@
+from functools import reduce
+
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+def sumaImpar(lista):
+    #lambda: duncion anonima
+    impar = filter(lambda x: x % 2, lista)
+    print(f'la lista de impares es: {list(impar)}')
+
+    suma = reduce(lambda x, y: x + y, lista)
+    print(f'la suma es: {suma}')
+
+sumaImpar(lista)
